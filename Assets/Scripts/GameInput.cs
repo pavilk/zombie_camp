f3d81575d408689bf977b14c025b726fcd0ff86f
pavilk <pkarenginp@gmail.com>
2025-03-18 20:17:07 +0500

    Исправление конфликтов Player.cs, Camera.cs, GameInput.cs

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,23 +16,3 @@ public class GameInput : MonoBehaviour
 
     public Vector2 MovementVector => playerInput.Player.Move.ReadValue<Vector2>();
 }
-=======
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class GameInput : MonoBehaviour
-{
-    private PlayerInputActions playerInput;
-
-    public static GameInput Instance { get; private set; }
-
-    private void Awake()
-    {
-        Instance = this;
-        playerInput = new PlayerInputActions();
-        playerInput.Enable();
-    }
-
-    public Vector2 MovementVector => playerInput.Player.Move.ReadValue<Vector2>();
-}
->>>>>>> 6c95146 (main)
